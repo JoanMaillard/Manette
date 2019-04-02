d964d2438003e148faecf521c6cc53f64738576a
AdelKahrimanovic <hoaxforde@gmail.com>
2019-04-02 15:10:30 +0200

    Update BLE_slave.ino

--- 08_Programmes/BLE_slave/BLE_slave.ino
+++ 08_Programmes/BLE_slave/BLE_slave.ino
@@ -184,13 +184,13 @@ void loop() {
 
     pRemoteCharacteristic->writeValue(dataBuffer, 8, 1); //write inputs inside of dataBuffer to BT server
     feedbackDataServ = pRemoteCharacteristicBack->readValue(); //read feedback data from server
-    Serial.println("read feedback data");
+   // Serial.println("read feedback data");
     for (byte i = 0; i < 2; i++) {
       feedbackData[i] = (uint8_t)feedbackDataServ[i];
-      Serial.print(feedbackData[i]);
-      Serial.print("  ");
+      //Serial.print(feedbackData[i]);
+      //Serial.print("  ");
     }
-    Serial.println(""); 
+    //Serial.println(""); 
     //TODO:  Treat feedback data (screen, buzzer and so on)
 
     /*Serial.println(millis());
