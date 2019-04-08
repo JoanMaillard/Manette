09da65b15092be765541011323ee0cbc0aefa1f6
Joan Maillard <joan@nerdgautica.net>
2019-04-08 11:32:45 +0200

    Doc

---
+++ 08_Programmes/BLE_client/BLE_client.ino
@@ -0,0 +1,202 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <HardwareSerial.h>
+
+#define BUFFER_SIZE 8
+#define WRITE_CONTROL 255
+
+byte dataBuffer[BUFFER_SIZE];
+HardwareSerial outSer(2);
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("b9d4de40-44be-11e9-b210-d663bd873d93");
+// The characteristic of the remote service we are interested in.
+static BLEUUID    charUUID("b9d4e282-44be-11e9-b210-d663bd873d93");
+static BLEUUID    backUUID("861c92c2-4ef6-11e9-8647-d663bd873d93");
+
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLERemoteCharacteristic* pRemoteCharacteristicBack;
+static BLEAdvertisedDevice* myDevice;
+
+//uint8_t val[8] = {0,0,0,0,0,0,0,0};//*/
+uint8_t retVal[2] = {0, 0}; //*/
+
+
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+  //Serial.print("Notify callback for characteristic ");
+  //Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+  //Serial.print(" of data length ");
+  //Serial.println(length);
+  //Serial.print("data: ");
+  //Serial.println((char*)pData);
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+    void onConnect(BLEClient* pclient) {
+    }
+
+    void onDisconnect(BLEClient* pclient) {
+      connected = false;
+      //Serial.println("onDisconnect");
+    }
+};
+
+bool connectToServer() {
+  //Serial.print("Forming a connection to ");
+  //Serial.println(myDevice->getAddress().toString().c_str());
+
+  BLEClient*  pClient  = BLEDevice::createClient();
+  //Serial.println(" - Created client");
+
+  pClient->setClientCallbacks(new MyClientCallback());
+
+  // Connect to the remove BLE Server.
+  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  //Serial.println(" - Connected to server");
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    //Serial.print("Failed to find our service UUID: ");
+    //Serial.println(serviceUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  //Serial.println(" - Found our service");
+
+
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+  pRemoteCharacteristicBack = pRemoteService->getCharacteristic(backUUID);
+  if (pRemoteCharacteristic == nullptr) {
+    //Serial.print("Failed to find our characteristic UUID: ");
+    //Serial.println(charUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  //Serial.println(" - Found our characteristic");
+
+  // Read the value of the characteristic.
+  if (pRemoteCharacteristic->canRead()) {
+    std::string value = pRemoteCharacteristic->readValue();
+    //Serial.print("The characteristic value was: ");
+    //Serial.println(value.c_str());
+  }
+  if (pRemoteCharacteristicBack->canRead()) {
+    std::string value = pRemoteCharacteristicBack->readValue();
+  }
+
+  if (pRemoteCharacteristic->canNotify())
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+  if (pRemoteCharacteristicBack->canNotify())
+    pRemoteCharacteristicBack->registerForNotify(notifyCallback);
+
+  connected = true;
+  return true;
+}
+/**
+   Scan for BLE servers and find the first one that advertises the service we are looking for.
+*/
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    /**
+        Called for each advertising BLE server.
+    */
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      //Serial.print("BLE Advertised Device found: ");
+      //Serial.println(advertisedDevice.toString().c_str());
+
+      // We have found a device, let us now see if it contains the service we are looking for.
+      if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+
+        BLEDevice::getScan()->stop();
+        myDevice = new BLEAdvertisedDevice(advertisedDevice);
+        doConnect = true;
+        doScan = true;
+
+      } // Found our server
+    } // onResult
+}; // MyAdvertisedDeviceCallbacks
+
+
+void setup() {
+  outSer.begin(115200, SERIAL_8N1);
+  Serial.begin(115200);
+  //pinMode(27, INPUT_PULLUP);
+  //Serial.println("Starting Arduino BLE Client application...");
+  BLEDevice::init("");
+
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+} // End of setup.
+
+
+// This is the Arduino main loop function.
+void loop() {
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer()) {
+      //Serial.println("We are now connected to the BLE Server.");
+
+    } else {
+      //Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+    doConnect = false;
+
+  }
+
+  if (connected) {
+    std::string feedbackDataServ;
+    uint8_t feedbackData[2] = {0, 0};
+    //Serial.println(millis());
+
+    outSer.write(WRITE_CONTROL); //signal inputs controller that the ESP is ready to read controller data
+    while (outSer.available() < 8) {} //do nothing as long as not all the serial inputs are ready to be read
+    outSer.readBytes(dataBuffer, BUFFER_SIZE); //read inputs from inputs controller and store into dataBuffer
+
+    //for (int i = 0; i < 8; i++) {
+    //Serial.print(dataBuffer[i]);
+    //Serial.print("   ");
+    // }
+    //Serial.println("");
+
+
+    // Set the characteristic's value to be the array of bytes that is actually a string.
+
+
+    pRemoteCharacteristic->writeValue(dataBuffer, 8, 1); //write inputs inside of dataBuffer to BT server
+    feedbackDataServ = pRemoteCharacteristicBack->readValue(); //read feedback data from server
+   // Serial.println("read feedback data");
+    for (byte i = 0; i < 2; i++) {
+      feedbackData[i] = (uint8_t)feedbackDataServ[i];
+      //Serial.print(feedbackData[i]);
+      //Serial.print("  ");
+    }
+    //Serial.println(""); 
+    //TODO:  Treat feedback data (screen, buzzer and so on)
+
+    /*Serial.println(millis());
+      Serial.println("----");//*/
+  } else if (doScan) {
+    BLEDevice::getScan()->start(5, false);  // this is just eample to start scan after disconnect, most likely there is better way to do it in arduino
+  }
+
+} // End of loop
---
+++ 08_Programmes/data_gatherer/data_gatherer.ino
@@ -0,0 +1,284 @@
+#define SDA_PORT PORTD
+#define SDA_PIN 6 // = A4
+#define SCL_PORT PORTD
+#define SCL_PIN 7 // = A5
+#include <SoftI2CMaster.h>
+
+#define iobusAddress 0
+#define pinaxis1 0
+#define pinaxis2 1
+#define pinaxis3 2
+#define pinaxis4 3
+#define pinaxis5 4
+#define pinaxis6 5
+#define DATA_SIZE 8
+#define I2C_7BITADDR 0x20
+
+
+const byte regA = 0x12;
+const byte regB = 0x13;
+byte potVal1, potVal2, joy1X, joy1Y, joy2X, joy2Y;
+int minIn1, minIn2, minInx3, minInx4, minIny3, minIny4, maxIn1, maxIn2, maxInx3, maxInx4, maxIny3, maxIny4, midX3, midY3, midX4, midY4;
+bool calibrating = false;
+bool calPressed = false;
+byte var;
+
+void setup() {
+  // put your setup code here, to run once:
+  minIn1 = 0;
+  minIn2 = 0;
+  minInx3 = 0;
+  minIny3 = 0;
+  minInx4 = 0;
+  minIny4 = 0;
+  maxIn1 = 1023;
+  maxIn2 = 1023;
+  maxInx3 = 1023;
+  maxIny3 = 1023;
+  maxInx4 = 1023;
+  maxIny4 = 1023;
+  Serial.begin(115200);
+  i2c_init();
+  if (!i2c_start((I2C_7BITADDR << 1) | I2C_WRITE)) { // start transfer
+    Serial.println("I2C device busy");
+    return;
+  }
+  i2c_write(0);
+  i2c_write(0xFF);
+  i2c_stop();
+  i2c_start((I2C_7BITADDR << 1) | I2C_WRITE);
+  i2c_write(1);
+  i2c_write(0xFF);
+  i2c_stop();
+  i2c_start((I2C_7BITADDR << 1) | I2C_WRITE);
+  i2c_write(0x0C);
+  i2c_write(0xFF);
+  i2c_stop();
+  i2c_start((I2C_7BITADDR << 1) | I2C_WRITE);
+  i2c_write(0x0D);
+  i2c_write(0xFF);
+  i2c_stop();
+  Serial.println("setup complete");
+  pinMode(pinaxis1, INPUT);
+  pinMode(pinaxis2, INPUT);
+  pinMode(pinaxis3, INPUT);
+  pinMode(pinaxis4, INPUT);
+  pinMode(pinaxis5, INPUT);
+  pinMode(pinaxis6, INPUT);
+  pinMode(8, OUTPUT);
+  //pinMode(9, INPUT_PULLUP);
+}
+
+void loop() {
+  var++;
+  byte *buttonData = getButtons();
+
+  calButCheck(buttonData[0]);
+
+
+  //if (!calibrating) {Serial.write(mainData,DATA_SIZE);}
+  //else {}
+  if (calibrating) {
+    Serial.println("Calibrating...");
+    digitalWrite(8, HIGH);
+    minIn1 = 0;
+    minIn2 = 0;
+    minInx3 = 0;
+    minIny3 = 0;
+    minInx4 = 0;
+    minIny4 = 0;
+    maxIn1 = 1023;
+    maxIn2 = 1023;
+    maxInx3 = 1023;
+    maxIny3 = 1023;
+    maxInx4 = 1023;
+    maxIny4 = 1023;
+    int temp[12] = {1023, 1023, 1023, 1023, 1023, 1023, 0, 0, 0, 0, 0, 0};
+    int tpotVal1, tpotVal2, tjoy1X, tjoy1Y, tjoy2X, tjoy2Y;
+    while (calibrating) {
+      byte *buttonData = getButtons();
+      calButCheck(buttonData[0]);
+      tpotVal1 = analogRead(pinaxis1);
+      tpotVal2 = analogRead(pinaxis2);
+      tjoy1X = analogRead(pinaxis3);
+      tjoy1Y = analogRead(pinaxis4);
+      tjoy2X = analogRead(pinaxis5);
+      tjoy2Y = analogRead(pinaxis6);
+      if (tpotVal1 < temp[0]) { //minimums
+        temp[0] = tpotVal1;
+      }
+      if (tpotVal2 < temp[1]) {
+        temp[1] = tpotVal2;
+      }
+      if (tjoy1X < temp[2]) {
+        temp[2] = tjoy1X;
+      }
+      if (tjoy1Y < temp[3]) {
+        temp[3] = tjoy1Y;
+      }
+      if (tjoy2X < temp[4]) {
+        temp[4] = tjoy2X;
+      }
+      if (tjoy2Y < temp[5]) {
+        temp[5] = tjoy2Y;
+      }
+      if (tpotVal1 > temp[6]) { //maximums
+        temp[6] = tpotVal1;
+      }
+      if (tpotVal2 > temp[7]) {
+        temp[7] = tpotVal2;
+      }
+      if (tjoy1X > temp[8]) {
+        temp[8] = tjoy1X;
+      }
+      if (tjoy1Y > temp[9]) {
+        temp[9] = tjoy1Y;
+      }
+      if (tjoy2X > temp[10]) {
+        temp[10] = tjoy2X;
+      }
+      if (tjoy2Y > temp[11]) {
+        temp[11] = tjoy2Y;
+      }
+    }
+    minIn1 = temp[0];
+    minIn2 = temp[1];
+    minInx3 = temp[2];
+    minIny3 = temp[3];
+    minInx4 = temp[4];
+    minIny4 = temp[5];
+    maxIn1 = temp[6] - 2;
+    maxIn2 = temp[7] - 2;
+    maxInx3 = temp[8] - 2;
+    maxIny3 = temp[9] - 2;
+    maxInx4 = temp[10] - 2;
+    maxIny4 = temp[11] - 2;
+    midX3 = analogRead(pinaxis3);
+    midY3 = analogRead(pinaxis4);
+    midX4 = analogRead(pinaxis5);
+    midY4 = analogRead(pinaxis6);
+  }
+  else {
+    digitalWrite(8, LOW);
+ 
+    if (Serial.read() == 255) {
+      getValues();
+      byte *mainData = conc(buttonData[0], buttonData[1]);
+      //mainData[1] = var;
+      Serial.write(mainData, 8);
+      /*if (!digitalRead(9)) {
+        Serial.println(mainData[1]);
+      }//*/
+      
+    }
+    /*Serial.print(mainData[0]);
+      Serial.print(", ");
+      Serial.print(mainData[1]);
+      Serial.print(", ");
+      Serial.print(mainData[2]);
+      Serial.print(", ");
+      Serial.print(mainData[3]);
+      Serial.print(", ");
+      Serial.print(mainData[4]);
+      Serial.print(", ");
+      Serial.print(mainData[5]);
+      Serial.print(", ");
+      Serial.print(mainData[6]);
+      Serial.print(", ");
+      Serial.println(mainData[7]);//*/
+  }
+}
+
+byte * getButtons() {
+  static byte vals[2];
+  if (!i2c_start((I2C_7BITADDR << 1) | I2C_WRITE)) { // start transfer
+    Serial.println("I2C device busy");
+    return;
+  }
+  i2c_write(regA);
+  i2c_rep_start((I2C_7BITADDR << 1) | I2C_READ); // restart for reading
+  byte val1 = i2c_read(true); // read one byte and send NAK to terminate
+  i2c_stop(); // send stop condition
+  if (!i2c_start((I2C_7BITADDR << 1) | I2C_WRITE)) { // start transfer
+    Serial.println("I2C device busy");
+    return;
+  }
+  i2c_write(regB);
+  i2c_rep_start((I2C_7BITADDR << 1) | I2C_READ); // restart for reading
+  byte val2 = i2c_read(true); // read one byte and send NAK to terminate
+  i2c_stop(); // send stop condition
+  vals[0] = val1;
+  vals[1] = val2;
+  return vals;
+}
+
+void getValues() {
+  int X1 = analogRead(pinaxis3);
+  int Y1 = analogRead(pinaxis4);
+  int X2 = analogRead(pinaxis5);
+  int Y2 = analogRead(pinaxis6);
+  potVal1 = map(constrain(analogRead(pinaxis1), minIn1, maxIn1), minIn1, maxIn1, 0, 255); //get axies constrained between their max values
+  potVal2 = map(constrain(analogRead(pinaxis2), minIn2, maxIn2), minIn2, maxIn2, 0, 255);
+  if (X1 <= midX3)                                                                                                                                  //map joysticks between 0 and 255 regardless of their calib inputs
+  {
+    joy1X = map(constrain(X1, minInx3, midX3), minInx3, midX3, 0, 127);
+  }
+  else
+  {
+    joy1X = map(constrain(X1, midX3, maxInx3), midX3, maxInx3, 128, 255);
+  }
+  if (Y1 <= midY3)
+  {
+    joy1Y = map(constrain(Y1, minIny3, midY3), minIny3, midY3, 0, 127);
+  }
+  else
+  {
+    joy1Y = map(constrain(Y1, midY3, maxIny3), midY3, maxIny3, 128, 255);
+  }
+  if (X2 <= midX4)
+  {
+    joy2X = map(constrain(X2, minInx4, midX4), minInx4, midX4, 0, 127);
+  }
+  else
+  {
+    joy2X = map(constrain(X2, midX4, maxInx4), midX4, maxInx4, 128, 255);
+  }
+  if (Y2 <= midY4)
+  {
+    joy2Y = map(constrain(Y2, minIny4, midY4), minIny4, midY4, 0, 127);
+  }
+  else
+  {
+    joy2Y = map(constrain(Y2, midY4, maxIny4), midY4, maxIny4, 128, 255);
+  }
+}
+
+void calButCheck(byte val1) {
+  if (!calPressed && !bitRead(val1, 0)) {
+    calPressed = true;
+    if (calibrating)
+    {
+      calibrating = false;
+    }
+    else {
+      calibrating = true;
+    }
+  }
+  else if (bitRead(val1, 0)) {
+    calPressed = false;
+  }
+}
+
+byte * conc(byte val1, byte val2) {
+  static byte concatenated[DATA_SIZE];
+  byte *buttonData = getButtons();
+  concatenated[0] = 255 - buttonData[0];
+  concatenated[1] = 255 - buttonData[1];
+  concatenated[2] = potVal1;
+  concatenated[3] = potVal2;
+  concatenated[4] = joy1X;
+  concatenated[5] = joy1Y;
+  concatenated[6] = joy2X;
+  concatenated[7] = joy2Y;
+  return concatenated;
+}
