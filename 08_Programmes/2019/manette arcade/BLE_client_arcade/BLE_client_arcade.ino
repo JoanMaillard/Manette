7843307a701fe671edd289a16d65d5f844b2e179
Joan Maillard <joanjoanjostorm@gmail.com>
2019-06-13 10:57:08 +0200

    Adaptation code pour console rétro

---
+++ 08_Programmes/2019/manette arcade/BLE_client_arcade/BLE_client_arcade.ino
@@ -0,0 +1,206 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <HardwareSerial.h>
+
+#define BUFFER_SIZE 2
+#define WRITE_CONTROL 255
+
+byte previousState = 0;
+
+byte dataBuffer[BUFFER_SIZE];
+HardwareSerial outSer(2);
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("b9d4de40-44be-11e9-b210-d663bd873d93"); //A faire matcher avec les UUID du serveur correspondant. UUID de service
+// The characteristic of the remote service we are interested in.
+static BLEUUID    charUUID("b9d4e282-44be-11e9-b210-d663bd873d93"); //A faire matcher avec les UUID du serveur correspondant. UUID de commande
+
+static boolean doConnect = false;
+static boolean connected = false;
+static boolean doScan = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
+
+/*
+ * 
+ * @func static notifyCallback callback notifier function for event handling (unused but necessary for the BLE client class to work)
+ * @param null
+ * @return void
+ * 
+ */
+
+static void notifyCallback(
+  BLERemoteCharacteristic* pBLERemoteCharacteristic,
+  uint8_t* pData,
+  size_t length,
+  bool isNotify) {
+}
+
+class MyClientCallback : public BLEClientCallbacks {
+
+/*
+ * 
+ * @func onConnect what happens when the client connects
+ * @param BLEClient* pointer to the target client instance
+ * @return void
+ * 
+ */
+  
+    void onConnect(BLEClient* pclient) {
+      connected = true;
+    }
+
+/*
+ * 
+ * @func onDisconnect what happens when the client disconnects or loses connection
+ * @param BLEClient* pointer to the target client instance
+ * @return void
+ * 
+ */
+
+    void onDisconnect(BLEClient* pclient) {
+      connected = false;
+      //Serial.println("onDisconnect");
+    }
+};
+
+/*
+ * 
+ * @func connectToServer connect the client to the server
+ * @param null
+ * @return bool 
+ * 
+ */
+
+bool connectToServer() {
+
+  BLEClient*  pClient  = BLEDevice::createClient();
+
+  pClient->setClientCallbacks(new MyClientCallback());
+
+  // Connect to the remove BLE Server.
+  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    pClient->disconnect();
+    return false;
+  }
+  //Serial.println(" - Found our service");
+
+
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
+  if (pRemoteCharacteristic == nullptr) {
+    pClient->disconnect();
+    return false;
+  }
+  //Serial.println(" - Found our characteristic");
+
+  // Read the value of the characteristic.
+  if (pRemoteCharacteristic->canRead()) {
+    std::string value = pRemoteCharacteristic->readValue();
+  }
+
+  if (pRemoteCharacteristic->canNotify())
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
+
+  /*if (pRemoteCharacteristicBack->canNotify())
+    pRemoteCharacteristicBack->registerForNotify(notifyCallback);*/
+
+  connected = true;
+  return true;
+}
+/**
+   Scan for BLE servers and find the first one that advertises the service we are looking for.
+*/
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    /**
+        Called for each advertising BLE server.
+    */
+
+    /*
+     * 
+     * @func onResult
+     * @param BLEAdvertisedDevice an advertised device found
+     * @return void
+     * 
+     */
+     
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+
+      // We have found a device, let us now see if it contains the service we are looking for.
+      if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
+
+        BLEDevice::getScan()->stop();
+        myDevice = new BLEAdvertisedDevice(advertisedDevice);
+        doConnect = true;
+        doScan = true;
+
+      } // Found our server
+    } // onResult
+}; // MyAdvertisedDeviceCallbacks
+
+/*
+ * 
+ * @func setup default setup function for arduino
+ * @param null
+ * @return void
+ * 
+ */
+
+void setup() {
+  delay(1000);
+  outSer.begin(115200, SERIAL_8N1);
+  BLEDevice::init("");
+
+  // Retrieve a Scanner and set the callback we want to use to be informed when we
+  // have detected a new device.  Specify that we want active scanning and start the
+  // scan to run for 5 seconds.
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setInterval(1349);
+  pBLEScan->setWindow(449);
+  pBLEScan->setActiveScan(true);
+  pBLEScan->start(5, false);
+  delay(1000);
+} // End of setup.
+
+/*
+ * 
+ * @func loop default loop function for arduino
+ * @param null
+ * @return void
+ * 
+ */
+
+// This is the Arduino main loop function.
+void loop() {
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  if (doConnect == true) {
+    if (connectToServer()) {
+
+    } else {
+    }
+    doConnect = false;
+
+  }
+
+  if (connected) {
+
+    outSer.write(WRITE_CONTROL); //signal inputs controller that the ESP is ready to read controller data
+    while (outSer.available() < 8) {} //do nothing as long as not all the serial inputs are ready to be read
+    outSer.readBytes(dataBuffer, BUFFER_SIZE); //read inputs from inputs controller and store into dataBuffer
+    
+    // Set the characteristic's value to be the array of bytes that is actually a string.
+    pRemoteCharacteristic->writeValue(dataBuffer, BUFFER_SIZE, 0); //write inputs inside of dataBuffer to BT server
+    
+  } else if (doScan) {
+    BLEDevice::getScan()->start(5, false);  // this is just example to start scan after disconnect, most likely there is better way to do it in arduino
+  }
+} // End of loop
---
+++ 08_Programmes/2019/manette arcade/BLE_server_arcade/BLE_server_arcade.ino
@@ -0,0 +1,157 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <HardwareSerial.h>
+
+#define SERVICE_UUID        "b9d4de40-44be-11e9-b210-d663bd873d93"              //Unique, à changer pour chaque serveur. UUID de service A CHANGER
+#define CHARACTERISTIC1_UUID "b9d4e282-44be-11e9-b210-d663bd873d93" //Unique, à changer pour chaque serveur. UUID de commande A CHANGER
+#define CHARACTERISTIC2_UUID "861c92c2-4ef6-11e9-8647-d663bd873d93"   //Unique, à changer pour chaque serveur. UUID de feedback A CHANGER
+
+BLECharacteristic *pCharacteristic1; // Pointer to the controller input characteristic
+BLECharacteristic *pCharacteristic2;
+BLEServer *pServer; // Pointer to the server instance
+uint8_t defaultInValues[2] = {0, 0};
+uint8_t ctrlInput[4] = {0};
+HardwareSerial outSer(0);
+bool serBackChanged = false;
+
+class MyCallbacks : public BLEServerCallbacks {
+  /*
+   * 
+   * @func onConnect Triggers on device connection. Changes connection interval to the shortest possible according to GATT standards.
+   * @param BLEServer* target BLE server
+   * @param esp_ble_gatts_cb_param_t* newly connected device
+   * @return void
+   * 
+   */
+    void onConnect(BLEServer* pServer, esp_ble_gatts_cb_param_t *param) {
+      //Serial.println("Device connected");
+      pServer->updateConnParams(param->connect.remote_bda,0x06, 0x07, 0, 10);
+      //Serial.println("Successfully updated params");
+    }
+
+    /*
+     * 
+     * @func onDisconnect Triggered on device disconnection. Does nothing.
+     * @param BLEServer* target BLE server
+     * @return void
+     * 
+     */
+
+    void onDisconnect(BLEServer* pServer) {
+      //Serial.println("Device disconnected");
+    }
+};
+
+class MyCtrlCharCallbacksCtrl1 : public BLECharacteristicCallbacks {
+
+  /*
+   * 
+   * @func onWrite Triggered when a remote client writes into a characteristic hosted by the server. 
+   * Outputs the received data to the writable buffer that gets transmitted upon request.
+   * @param BLECharacteristic* target BLE GATT characteristic
+   * @return void
+   * 
+   */
+   
+    void onWrite(BLECharacteristic* pChar) {
+      //uint8_t ctrlInput[8] = {0};
+      uint8_t* pCharData;
+      //Serial.println(millis());
+      pCharData = pChar->getData();
+      //Serial.println(millis());
+      for (byte i = 0; i < 2; i++) {
+        ctrlInput[i] = pCharData[i];
+        //Serial.print(ctrlInput[i], BIN);
+        //Serial.print("   ");
+      }
+      //outSer.write(ctrlInput, 8);
+      //Serial.println("");
+      
+    }
+};
+
+class MyCtrlCharCallbacksCtrl2 : public BLECharacteristicCallbacks {
+
+  /*
+   * 
+   * @func onWrite Triggered when a remote client writes into a characteristic hosted by the server. 
+   * Outputs the received data to the writable buffer that gets transmitted upon request.
+   * @param BLECharacteristic* target BLE GATT characteristic
+   * @return void
+   * 
+   */
+   
+    void onWrite(BLECharacteristic* pChar) {
+      //uint8_t ctrlInput[8] = {0};
+      uint8_t* pCharData;
+      //Serial.println(millis());
+      pCharData = pChar->getData();
+      //Serial.println(millis());
+      for (byte i = 0; i < 2; i++) {
+        ctrlInput[i+2] = pCharData[i];
+        //Serial.print(ctrlInput[i], BIN);
+        //Serial.print("   ");
+      }
+      //outSer.write(ctrlInput, 8);
+      //Serial.println("");
+      
+    }
+};
+
+/*
+ * 
+ * @func setup Default arduino setup function. Sets up the serial towards controllable obect (SPI). Initializes the BLE server.
+ * @param null
+ * @return void
+ * 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+  outSer.begin(115200, SERIAL_8N1);
+
+  BLEDevice::init("ESP1");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic1 = pService->createCharacteristic(
+                      CHARACTERISTIC1_UUID,
+                      BLECharacteristic::PROPERTY_READ |
+                      BLECharacteristic::PROPERTY_WRITE
+                    );
+  pCharacteristic1->setCallbacks(new MyCtrlCharCallbacksCtrl1);
+  pCharacteristic1->setValue(defaultInValues, 2);
+  pCharacteristic2 = pService->createCharacteristic(
+                          CHARACTERISTIC2_UUID,
+                          BLECharacteristic::PROPERTY_READ |
+                          BLECharacteristic::PROPERTY_WRITE
+                        );
+  
+  pCharacteristic2->setCallbacks(new MyCtrlCharCallbacksCtrl2);
+  pCharacteristic2->setValue(defaultInValues, 2);
+  pService->start();
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // set minimum connection interval to 6x1.25ms
+  pAdvertising->setMaxPreferred(0x12); // set maximum connection interval to 10x1.25ms
+  BLEDevice::startAdvertising();
+  //Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+/*
+ * 
+ * @func loop Default arduino loop function. Takes care of checking the serial buffer for a data request in the form
+ * of feedback data and sends the response over. Then transmits the feedback data to the remote client.
+ * @param null
+ * @return void
+ * 
+ */
+ 
+void loop() {
+  if (outSer.read()==255){
+  outSer.write(ctrlInput,4);
+  }
+}
